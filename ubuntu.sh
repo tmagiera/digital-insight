21f8fb56921294202e2f5fbf4cbaa3b8d5829149
Tadeusz Magiera <tadeusz.magiera@accordgroup.co.uk>
2013-09-25 06:22:22 -0700

    Initial Vagrant config

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,1 +1,5 @@
-sudo apt-get instal php5-cli curl php5-json
+# list of commands to perform before start on Ubuntu 13.10
+
+sudo apt-get install php5-cli php5-json git curl
+# install vagrant
+# install virtualbox

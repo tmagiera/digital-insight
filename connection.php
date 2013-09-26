8a3a4670a800ce2828f9889010f2dfa514b4f243
vagrant <vagrant@precise64.(none)>
2013-09-26 15:18:26 +0000

    ODBC preparation

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: TMagiera
+ * Date: 26/09/13
+ * Time: 15:56
+ * To change this template use File | Settings | File Templates.
+ */
+$db = new PDO('odbc:DSN=accord_dev;UID=ciuser;PWD=manicminer');
+
